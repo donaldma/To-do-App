32d02ef3cc654e0116b6c3b75a618af9311316be
donaldma <donald.ma@hotmail.ca>
2017-09-12 04:49:03 -0700

    Nav Bar into a Component

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ReduxPromise from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
+import NavBar from './components/nav';
 
 import reducers from './reducers';
 import AllTasks from './components/tasks_all';
@@ -16,6 +17,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>  
     <BrowserRouter>
       <div>
+        <NavBar />
         <Switch>
           <Route path="/tasks" component={AllTasks} />
           <Route path="/" component={UserTasks} />
